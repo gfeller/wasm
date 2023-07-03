73ef052b615e70880aeeed56d11f7dbaebd7008f
Michael Gfeller <michael.gfeller@ost.ch>
2023-07-03 16:56:43 +0200

    removed some dead code

--- index.js
+++ index.js
@@ -1,93 +1,27 @@
-// Import our outputted wasm ES6 module
-// Which, export default's, an initialization function
 import init from "./pkg/wasm_demo.js";
-import {Universe, Cell} from "./pkg/wasm_demo.js"; 
+import {SampleQrCode} from "./pkg/wasm_demo.js"; 
 
 
-const CELL_SIZE = 5; // px
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
-
-
-
-const runWasm = async () => {
-  // Instantiate our wasm module
-  const module = await init("./pkg/wasm_demo_bg.wasm");
-
-  
-  const universe = Universe.new();
-  const width = universe.width();
-  const height = universe.height();
+function printQrCode(elem, memory, imagePointer, width, height) {
+  let imageData = new ImageData(new Uint8ClampedArray(memory, imagePointer, width * height * 4), width, height);
+  elem.width = width;
+  elem.height = height
+  const ctx = elem.getContext('2d');
  
-  const canvas = document.getElementById("game-of-life-canvas");
-  canvas.height = (CELL_SIZE + 1) * height + 1;
-  canvas.width = (CELL_SIZE + 1) * width + 1;
-  
-  const ctx = canvas.getContext('2d');
-  
+  ctx.putImageData(imageData, 0, 0);
+}
 
+const module = await init("./pkg/wasm_demo_bg.wasm");
 
-  const drawGrid = (ctx) => {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-  
-    // Vertical lines.
-    for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-    }
-  
-    // Horizontal lines.
-    for (let j = 0; j <= height; j++) {
-      ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
-      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-    }
-  
-    ctx.stroke();
-  };
-  
-  const getIndex = (row, column) => {
-    return row * width + column;
-  };
-  
-  const drawCells = (ctx, universe, memory) => {
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-  
-    ctx.beginPath();
-  
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < width; col++) {
-        const idx = getIndex(row, col);
-  
-        ctx.fillStyle = cells[idx] === Cell.Dead
-          ? DEAD_COLOR
-          : ALIVE_COLOR;
-  
-        ctx.fillRect(
-          col * (CELL_SIZE + 1) + 1,
-          row * (CELL_SIZE + 1) + 1,
-          CELL_SIZE,
-          CELL_SIZE
-        );
-      }
-    }
-  
-    ctx.stroke();
-  };
-  
-  const renderLoop = () => {
-    universe.tick();
-  
-    drawGrid(ctx);
-    drawCells(ctx, universe, module.memory);
-  
-    requestAnimationFrame(renderLoop);
-  };
-
-  requestAnimationFrame(renderLoop);
- 
- 
+const generateQrCode = async (value) => {
+  const qrCode = SampleQrCode.new(value || "Hello World"); 
+  printQrCode( document.getElementById("qr-code"), module.memory.buffer, qrCode.buffer(), qrCode.width(), qrCode.height());
 };
-runWasm();
+
+
+
+document.getElementById("qr-text").addEventListener("change", (event)=>{
+  generateQrCode(event.target.value);
+})
+
+generateQrCode();
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use std::fmt;
 
+ 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 
 #[wasm_bindgen]
 #[repr(u8)]
@@ -18,122 +17,62 @@ pub enum Cell {
     Alive = 1,
 }
 
+
 #[wasm_bindgen]
-pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
+pub struct SampleQrCode {
+    width: u16,
+    height: u16,
+    buffer: Vec<u8>
 }
- 
 
 
 
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
+#[wasm_bindgen]
+impl SampleQrCode {
+    pub fn new(value: &str) -> SampleQrCode {
+
+        console_error_panic_hook::set_once();
+
+
+        let qr_code = qr_code::QrCode::new(value).unwrap();
+
+        let height: u16 = qr_code.width() as u16;
+        let width: u16 = qr_code.width() as u16;
+        let size: usize = (height * width * 4).into();
+
+
+        let mut buffer: Vec<u8> = Vec::new();
+        buffer.resize(size, 0);
+        
+         // (0..size).collect::<Vec<u8>>();
+       
+        let mut pos = 0;
+        for pixel_set in qr_code.into_colors() {
+            let pixel_value = if pixel_set == qr_code::Color::Light { 255 } else { 0 };
+            buffer[pos] = pixel_value;   // Red
+            buffer[pos+1] = pixel_value; // Green
+            buffer[pos+2] = pixel_value; // Blue
+            buffer[pos+3] = 255;         // Alpha
+            pos += 4;
         }
 
-        Ok(())
-    }
-}
-
-#[wasm_bindgen]
-impl Universe {
-    pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
-        Universe {
+        SampleQrCode { 
             width,
             height,
-            cells,
+            buffer
         }
+        
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
-    }
-
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
-            }
-        }
-
-        self.cells = next;
-    }
-
-
-
-    pub fn width(&self) -> u32 {
+    pub fn width(&self) -> u16 {
         self.width
     }
 
-    pub fn height(&self) -> u32 {
+    pub fn height(&self) -> u16 {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-    
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
+    pub fn buffer(&self) -> *const u8 {
+        self.buffer.as_ptr()
     }
 }
